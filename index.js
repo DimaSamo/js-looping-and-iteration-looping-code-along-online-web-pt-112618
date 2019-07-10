66228ab0e2ecfd6c606822b63cd505778a4b0aad
Dmitri Samotessov <dimasamo96@gmail.com>
2019-07-10 21:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function writeCards(array, eventName){
   let retArray=[]
  for (var i = 0; i < array.length; i++) {
-   retArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift.`)
+   retArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
  }
  return retArray
 }

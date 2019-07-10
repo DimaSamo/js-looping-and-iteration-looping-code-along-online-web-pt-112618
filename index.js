bca74870d51977e2d2297b5882a29e8efed7ae1e
Dmitri Samotessov <dimasamo96@gmail.com>
2019-07-10 21:56:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function writeCards(array, eventName){
   let retArray=[]
- for (var i = 0; i < array.length; i++) {
+ for (let i = 0; i < array.length; i++) {
    retArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
  }
  return retArray
